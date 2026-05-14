8f6158c26fa15158acb888e5ce6c99e6ce3d395a
Zefek <32429493+Zefek@users.noreply.github.com>
2026-05-14 07:41:07 +0200

    Migrace na V6 (#10)
    * Migrace na V6
    
    * Diagnostika (#11)
    
    * Diagnostika
    
    * Konfigurace
    
    * Arduino config
    
    * Submodule  a sketch (#12)

--- config_default.h
+++ config_default.h
@@ -1,11 +1,4 @@
-#define WifiSSID "WifiSSID"
-#define WifiPassword "WifiPassword"
-#define MQTTUsername "MQTTUsername"
-
-#define MQTTPassword "*MQTTPassword"
-
-#define MQTTHost "MQTTHost"
-
 #define GARAGE_STATE "GARAGE_STATE"
 #define GARAGE_CMD "GARAGE_CMD"
 #define GARAGE_TEMPERATURE "GARAGE_TEMPERATURE"
+#define GARAGE_DIAG "GARAGE_DIAG"
